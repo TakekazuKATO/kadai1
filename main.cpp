ee6829b872877a5a31bdb8642e2f4f561a6682ab
Takekazu KATO <takekazu.kato@gmail.com>
2018-04-04 10:05:24 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int sum=0;//計算結果を入れる変数
+
+    /*
+     * ここに１から１００まで足してsumに入れるプログラムを書く
+     */
+
+
+    printf("sum of 1 to 100 = %d\n",sum);
+    return 0;
+}
